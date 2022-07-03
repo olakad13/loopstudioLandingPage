4b2b92efa9741d7b735357e89e6e5e3a81c312d3
Your Name <you@example.com>
2022-07-03 01:18:32 +0100

    Adding more styles and script

--- Js/index.js
+++ Js/index.js
@@ -2,13 +2,16 @@ let hamburger = document.getElementById('hamburger-menu');
 let navCard = document.getElementsByClassName('nav-links-container')[0] ;
 let closeButton = document.getElementById('close-button');
 
+hamburger.addEventListener('click', displayNavCard);
+closeButton.addEventListener('click', closeNavCard);
 
 function displayNavCard() {
-    navCard.style.display = 'flex';
-};
+    navCard.style.opacity = 1;
+    navCard.style.zIndex = 100;
+}
 
 function closeNavCard() {
-    // navCard.removeEventListener("click",displayNavCard())
-    navCard.style.display = 'none';
-};
+    navCard.style.opacity = 0;
+    navCard.style.zIndex = -100;
+}
 
